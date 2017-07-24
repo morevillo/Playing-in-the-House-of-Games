8d0b91da57efd9bf0856cd94f400e0b095eeabf5
Miguel Orevillo <mlgorevillo@yahoo.com>
2017-07-25 11:13:24 +1200

    WIP: Login and Register

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var path = require('path');
+var cors = require('cors');
 
 //Passport
 var passport = require('passport');
@@ -21,6 +22,43 @@ app.use(session({
 	}
 }));
 
+// app.all('/*',function(req, res, next) {  
+//       res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+//       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
+//       next();
+//  });  
+// app.use(function (req, res, next) {
+
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+
+//     // Pass to next layer of middleware
+//     next();
+// });
+app.use(function(req, res, next) {
+	if(req.headers.origin){
+		res.header("Access-Control-Allow-Origin", "*");
+	  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	  	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");	
+  		if(req.method==='OPTIONS'){return res.send(200)}
+	}
+	
+  	next();
+});
+
+app.use(cors());
+//app.options('*', cors());
+
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -30,8 +68,8 @@ var pg = require('pg');
 
 const Pool = require('pg-pool');
 const url = require('url')
- 
-const params = url.parse(process.env.DATABASE_URL);
+const connectionString = "postgres://nldtjebakdcwid:fd27e6eeeaa90bc9f691d2692d4d1c5632005215ba880f58fa5d0944bdbc428f@ec2-23-21-220-188.compute-1.amazonaws.com:5432/df6jlcpaevc1fe";
+const params = url.parse(process.env.DATABASE_URL || connectionString);
 const auth = params.auth.split(':');
  
 const config = {
@@ -76,12 +114,11 @@ app.use('/css', express.static(__dirname + '/public/css'));
 
 //=============Passport Session Setup=================//
 passport.serializeUser(function(user, done) {
+	console.log("USER: " + user)
     done(null, user);
 });
-passport.deserializeUser(function(obj, done) {
-    pool.query("SELECT * FROM users WHERE id = ($1)", [id], function(err, result){
-    	done(err, result.rows[0]);
-    });
+passport.deserializeUser(function(user, done) {
+	done(null, user);
 });
 
 //Passport Login
@@ -90,9 +127,20 @@ passport.use('local-login', new LocalStrategy({
 	passwordField: 'password',
 	passReqToCallback : true
 }, function(req, username, password, done){
+	console.log("Entering login strategy");
+	pool.query("SELECT * FROM users WHERE username = $1", [username], function(err, msg, result){
+		console.log("Querying for login");
+		if(err){
+			console.log("Unsuccessfull Login :(");
+			return done(err);
+		}else{
+			return done(null, result.rows[0]);
+		}
+	});
 	//Query for login will be here
 	//var user = client.query("SELECT * FROM [] WHERE username = '" + username + "';", callback);
 	//brcypt stuff Callback function
+
 }));
 
 //Passport Register/Signup
@@ -103,87 +151,41 @@ passport.use('local-signup', new LocalStrategy({
 }, function(req, username, password, done){
 	process.nextTick(function(){
 		//DO queries here?
-		pool.query("SELECT * FROM users where uname = ($1)", [req.body.username], function(err, result){
+		console.log("Querying now...");
+		pool.query("SELECT * FROM users where uname = $1", [req.body.username], function(err, result){
+			// console.log("Querying now...");
 				if(err){
 					//client.release();
 					return done(err);
 				}else{
-					var values = [req.body.fname, req.body.lname, req.body.uname, req.body.email, req.body.age, req.body.gender, req.body.ethnicity, req.body.password];
-					pool.query("INSERT INTO users (fname, lname, uname, email, gender, age, ethnicity, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], function(err, result){
-						//client.release();
-						if(err){
-							return done(err);
-						}else{
-							console.log("SUCESSFULLY ADDED USER!!");
-						}
-					});
+					if(result.rows[0] != undefined){
+						// already exists
+						console.log("Already exists");
+						return done(null, false, request.flash('signupMessage', 'That username is already taken.'));						
+					} else {
+						var user = {
+							username: username,
+							password: password
+						};
+
+						console.log("Doesn't exist");
+						var values = [req.body.fname, req.body.lname, username, req.body.email, req.body.gender, req.body.age, req.body.ethnicity, req.body.password];
+						pool.query("INSERT INTO users (fname, lname, uname, email, gender, age, ethnicity, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", values, function(err, result){
+							//client.release();
+							if(err){
+								return done(err);
+							}else{
+								console.log("SUCESSFULLY ADDED USER!!");
+								
+								return done(null, user);
+							}
+						});
+					}
 				}
 			});
-		// pool.connect().then((client, err) => {
-		// 	if(err){
-		// 		client.release();
-		// 		return done(err);
-		// 	}
 
-		// 	client.query("SELECT * FROM users where uname = ($1)", [req.body.username], function(err, result){
-		// 		if(err){
-		// 			client.release();
-		// 			return done(err);
-		// 		}
-
-		// 		if(result.rows.length){
-		// 			client.release();
-		// 			return done(null, false, req.flash('signupMessage', "Email already has an account"));
-		// 		}else{
-		// 			var values = [req.body.fname, req.body.lname, req.body.uname, req.body.email, req.body.age, req.body.gender, req.body.ethnicity, req.body.password];
-		// 			client.query("INSERT INTO users (fname, lname, uname, email, gender, age, ethnicity, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7], function(err, result){
-		// 				client.release();
-		// 				if(err){
-		// 					return done(err);
-		// 				}
-		// 			});
-		// 		}
-		// 	});
-		// });
 	});
 }));
-
-// passport.use('local-register', new LocalStrategy({
-// 	usernameField: 'username',
-// 	passwordField: 'password',
-// 	passReqToCallback : true
-// }, function(req, username, password, done){
-// 	process.nextTick(function(){
-// 		console.log("Registering new user now!");
-// 		var user = client.query("SELECT * FROM users WHERE username = '" + username + "';", callback);
-// 		function callback(err, res){
-// 			if (res.rows[0] != undefined){ //if username already exists
-// 				console.log("Username already exists, check if password matches using bcrypt?");
-// 			}else{
-// 				console.log("Adding new suer to database");
-// 				var values = [req.body.fname, req.body.lname, req.body.uname, req.body.email, req.body.age, req.body.gender, req.body.ethnicity, req.body.password]
-// 				for (var i = 0; i<values.length; i++) {
-// 					console.log("User information " + values[i]);
-// 				}
-// 				var query = client.query("INSERT INTO users (fname, lname, uname, email, gender, age, ethnicity, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
-// 				req.session.username = username;
-// 				req.session.total = 0;
-// 				req.session.save();
-// 				return done(null, user);
-// 			}
-// 			return done(null, false);
-// 		}
-// 	 // 	var values = [req.body.fname, req.body.lname, req.body.uname, req.body.email, req.body.age, req.body.gender, req.body.ethnicity, req.body.password]
-// 		// var query = client.query("INSERT INTO users (fname, lname, uname, email, gender, age, ethnicity, password) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", values[0], values[1], values[2], values[3], values[4], values[5], values[6], values[7]);
-// 		// // var user = client.query("SELECT * FROM users WHERE ");
-// 		// if(res.rows[0]!=undefined){
-
-// 		// }else{
-// 		// 	var query = client.query("INSERT INTO users (fname, lname, uname, email, gender, age, ethnicity, password) VALUES")
-// 		// }
-// 	});
-// }));
-
 
 //===================ROUTES===========================//
 
@@ -194,62 +196,45 @@ app.get('/', function(req, res) {
 });
 
 app.get('/register', function(req, res){
+	//res.setHeader('Cache-Control', 'public, max-age= 3600');
 	res.render('pages/register', { message: req.flash('signupMessage') });
 });
 
-// app.post('/register', passport.authenticate('local-register', {
+app.post('/register/auth', passport.authenticate('local-signup', {
+        successRedirect : '/pregame', // redirect to the secure profile section
+        failureRedirect : '/register', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+}));
 
+// app.post('/register/auth', function (req, res, next){
+// 	passport.authenticate('local-signup', function(err, user, info){
+// 		if(err){
+// 			return next(err);
+// 		}if (user){
+// 			res.send(200);
+// 		}else{
+// 			res.send('Registration unsuccessful');
+// 			return;
+// 		}
+// 	})(req, res, next)
 // });
-
-// app.post('/register/auth', passport.authenticate('local-register', {
-//         successRedirect : '/pregame', // redirect to the secure profile section
-//         failureRedirect : '/register', // redirect back to the signup page if there is an error
-//         failureFlash : true // allow flash messages
-// }));
-
-// app.post('/register/auth', passport.authenticate('local-register', {
-//         successRedirect : '/pregame', // redirect to the secure profile section
-//         failureRedirect : '/register', // redirect back to the signup page if there is an error
-//         failureFlash : true // allow flash messages
-// }));
-
-app.post('/register/auth', function (req, res, next){
-	passport.authenticate('local-signup', function(err, user, info){
-		if(err){
-			return next(err);
-		}if (user){
-			res.send(200);
-		}else{
-			res.send('Registration unsuccessful');
-			return;
-		}
-	})(req, res, next)
-});
-
 
 app.get('/disclaimer',function(req, res){
 	res.render('pages/disclaimer');
 });
 
-
-app.get('/pregame', function(req, res){
+app.get('/pregame', isLoggedIn, function(req, res){
 	res.render('pages/pregame');
 });
 
-// app.get('/pregame', isLoggedIn, function(req, res){
-// 	res.render('pages/pregame');
-// });
+function isLoggedIn(req, res, next) {
 
-// function isLoggedIn(req, res, next) {
-
-//     // if user is authenticated in the session, carry on 
-//     if (req.isAuthenticated())
-//         return next();
-
-//     // if they aren't redirect them to the home page
-//     res.redirect('/');
-// }
-
+    // if user is authenticated in the session, carry on 
+    if (req.isAuthenticated())
+        return next();
+    // if they aren't redirect them to the home page
+    res.redirect('/');
+}
 
 app.get('/gameinfo', function(req, res){
 	res.render('pages/gameinfo');
@@ -259,7 +244,7 @@ app.get('/game', function(req, res){
 	res.render('pages/game');
 });
 
-app.get('/postgame', function(req, res){
+app.get('/postgame', isLoggedIn, function(req, res){
 	res.render('pages/postgame');
 });
 
@@ -267,9 +252,11 @@ app.get('/login', function(req, res){
 	res.render('pages/login', { message: req.flash('loginMessage') });
 });
 
-//app.post('/login', function(req, res){
-
-// });
+app.post('/login/auth', passport.authenticate('local-login', {
+    successRedirect : '/pregame', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
 
 app.get('/logout', function(req, res){
 	req.logout();
